3a0c5f71979fe5ebd0c5425a65386048c7e1c97f
chencaleb <clb_chen@berkeley.edu>
2016-02-23 19:06:29 -0800

    css for home/about

--- coffee_hop/app/models/user.rb
+++ coffee_hop/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates :email, uniqueness: true
+	validates :username, uniqueness: true
 
 	def self.confirm(params)
 		@user = User.find_by({email: params[:email]})
