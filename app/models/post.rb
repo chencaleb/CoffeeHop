d1e14a0657ba7e43c90867cc265d4c43a1c70b1d
chencaleb <clb_chen@berkeley.edu>
2016-03-02 15:50:42 -0800

    fixed bugs

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :coffeeshop
 
-	validates :fullness, numericality: { greater_than_or_equal_to: 0, 
+	validates :fullness, :description, numericality: { greater_than_or_equal_to: 0, 
                                     less_than_or_equal_to: 100,
                                     message: 'not a valid fullness' }
 end
